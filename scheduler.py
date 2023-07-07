154d8e3bc391c9b48b444e50faaac29b44329d6b
dalamat <squalldmitri@gmail.com>
2023-07-07 20:17:56 +0300

    Added grace period to scheduler

--- scheduler.py
+++ scheduler.py
@@ -57,24 +57,25 @@ if __name__ == "__main__":
     scheduler = BackgroundScheduler(executors=executors)
 
     # Add the jobs to the scheduler
+    misfire_grace_time = 60 # Specifies time in seconds within which the task can be executed in case of delay for various reasons
     
     # Run every hour at 25 and 55 minutes from 6 to 23 every day
-    scheduler.add_job(task_update_groups_full, 'cron', minute='25,55', hour='6-22')
-    scheduler.add_job(task_update_events_filter, 'cron', minute='25,55', hour='6-22')
-    scheduler.add_job(task_update_invoices_filter, 'cron', minute='25,55', hour='6-22')
-    scheduler.add_job(task_update_students_filter, 'cron', minute='25,55', hour='6-22')
-    scheduler.add_job(task_update_leads_full, 'cron', minute='25,55', hour='6-22')
-    scheduler.add_job(task_update_budgets_full, 'cron', minute='25,55', hour='6-22')
+    scheduler.add_job(task_update_groups_full, 'cron', minute='25,55', hour='6-22', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_events_filter, 'cron', minute='25,55', hour='6-22', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_invoices_filter, 'cron', minute='25,55', hour='6-22', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_students_filter, 'cron', minute='25,55', hour='6-22', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_leads_full, 'cron', minute='25,55', hour='6-22', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_budgets_full, 'cron', minute='25,55', hour='6-22', misfire_grace_time=misfire_grace_time)
 
     # Run once a day at midnight
-    scheduler.add_job(task_update_groups_full, 'cron', minute='0', hour='0')
-    scheduler.add_job(task_update_events_full, 'cron', minute='0', hour='0')
-    scheduler.add_job(task_update_invoices_full, 'cron', minute='0', hour='0')
-    scheduler.add_job(task_update_students_full, 'cron', minute='0', hour='0')
-    scheduler.add_job(task_update_leads_full, 'cron', minute='0', hour='0')
+    scheduler.add_job(task_update_groups_full, 'cron', minute='0', hour='0', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_events_full, 'cron', minute='0', hour='0', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_invoices_full, 'cron', minute='0', hour='0', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_students_full, 'cron', minute='0', hour='0', misfire_grace_time=misfire_grace_time)
+    scheduler.add_job(task_update_leads_full, 'cron', minute='0', hour='0', misfire_grace_time=misfire_grace_time)
 
     # Run hourly
-    scheduler.add_job(heartbeat, 'cron', minute='0', hour='0-23')
+    scheduler.add_job(heartbeat, 'cron', minute='0', hour='0-23', misfire_grace_time=misfire_grace_time)
 
     # Start the scheduler
     scheduler.start()
