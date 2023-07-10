157753648afb9e5115f59002999798958413d867
dalamat <squalldmitri@gmail.com>
2023-07-10 21:47:29 +0300

    Added a template for setting env_vars in windows

---
+++ set_env_var.bat
@@ -0,0 +1,17 @@
+@echo off
+setx database "database"
+setx db_user "db_user"
+setx db_password "db_password"
+
+setx bo_login "bo_login"
+setx bo_password "bo_password"
+
+setx amo_email "amo_email"
+setx amo_password "amo_password"
+setx amo_host "amo_host"
+
+setx gcp_spreadsheet_id "gcp_spreadsheet_id"
+
+setx telegram_bot_token "telegram_bot_token"
+setx telegram_group "telegram_group"
+setx telegram_mentions "telegram_mentions"
