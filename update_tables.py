f48e1f0dc0e2ba8684e67bc46c7372a5c835f2bf
Dmitry <squalldmitri@gmail.com>
2023-07-31 18:14:45 +0300

    Removed the silent options from parameter_sets

--- update_tables.py
+++ update_tables.py
@@ -10,26 +10,17 @@ from url_generator import get_urls
 
 
 PARAMETER_SETS = {
-    "events_full": {"csv_url":paths.bo_csv_url_events,"output_path":paths.events_csv_path,"table_name":"EVENTS FULL","db_script_function":"refresh_db_events","send_messages":True},
-    "groups_full":{"csv_url":paths.bo_csv_url_groups,"output_path":paths.groups_csv_path,"table_name":"GROUPS FULL","db_script_function":"refresh_db_groups","send_messages":True},
-    "invoices_full":{"csv_url":paths.bo_csv_url_invoices,"output_path":paths.invoices_csv_path,"table_name":"INVOICES FULL","db_script_function":"refresh_db_invoices","send_messages":True},
-    "students_full":{"csv_url":paths.bo_csv_url_students,"output_path":paths.students_csv_path,"table_name":"STUDENTS FULL","db_script_function":"refresh_db_students","send_messages":True},
-    "events_filter":{"csv_url":paths.bo_csv_url_events_filter,"output_path":paths.events_filter_csv_path,"table_name":"EVENTS UPDATES","db_script_function":"update_db_events","send_messages":True},
-    "invoices_filter":{"csv_url":get_urls("invoices_filter"),"output_path":paths.invoices_filter_csv_path,"table_name":"INVOICES UPDATES","db_script_function":"update_db_invocies","send_messages":True},
-    "students_filter":{"csv_url":get_urls("students_filter"),"output_path":paths.students_filter_csv_path,"table_name":"STUDENTS UPDATES","db_script_function":"update_db_students","send_messages":True},
-    "events_full_silent": {"csv_url":paths.bo_csv_url_events,"output_path":paths.events_csv_path,"table_name":"EVENTS FULL","db_script_function":"refresh_db_events","send_messages":False},
-    "groups_full_silent":{"csv_url":paths.bo_csv_url_groups,"output_path":paths.groups_csv_path,"table_name":"GROUPS FULL","db_script_function":"refresh_db_groups","send_messages":False},
-    "invoices_full_silent":{"csv_url":paths.bo_csv_url_invoices,"output_path":paths.invoices_csv_path,"table_name":"INVOICES FULL","db_script_function":"refresh_db_invoices","send_messages":False},
-    "students_full_silent":{"csv_url":paths.bo_csv_url_students,"output_path":paths.students_csv_path,"table_name":"STUDENTS FULL","db_script_function":"refresh_db_students","send_messages":False},
-    "events_filter_silent":{"csv_url":paths.bo_csv_url_events_filter,"output_path":paths.events_filter_csv_path,"table_name":"EVENTS UPDATES","db_script_function":"update_db_events","send_messages":False},
-    "invoices_filter_silent":{"csv_url":get_urls("invoices_filter"),"output_path":paths.invoices_filter_csv_path,"table_name":"INVOICES UPDATES","db_script_function":"update_db_invocies","send_messages":False},
-    "students_filter_silent":{"csv_url":get_urls("students_filter"),"output_path":paths.students_filter_csv_path,"table_name":"STUDENTS UPDATES","db_script_function":"update_db_students","send_messages":False},
-    "leads_full":{"csv_url":None,"output_path":paths.leads_csv_path,"table_name":"LEADS FULL","db_script_function":"refresh_db_leads","source":"AMO","send_messages":True},
-    "leads_full_silent":{"csv_url":None,"output_path":paths.leads_csv_path,"table_name":"LEADS FULL","db_script_function":"refresh_db_leads","source":"AMO","send_messages":False},
-    "budgets_full":{"csv_url":None,"output_path":paths.budgets_csv_path,"table_name":"BUDGETS FULL","db_script_function":"refresh_db_budgets","source":"GCP","send_messages":True},
-    "budgets_full_silent":{"csv_url":None,"output_path":paths.budgets_csv_path,"table_name":"BUDGETS FULL","db_script_function":"refresh_db_budgets","source":"GCP","send_messages":False},
-    "payments_full":{"csv_url":paths.bo_csv_url_payments,"output_path":paths.payments_csv_path,"table_name":"PAYMENTS FULL","db_script_function":"refresh_db_payments","send_messages":True},
-    "payments_filter":{"csv_url":get_urls("payments_filter"),"output_path":paths.payments_filter_csv_path,"table_name":"PAYMENTS UPDATES","db_script_function":"update_db_payments","send_messages":True}
+    "events_full": {"csv_url":paths.bo_csv_url_events,"output_path":paths.events_csv_path,"table_name":"EVENTS FULL","db_script_function":"refresh_db_events"},
+    "groups_full":{"csv_url":paths.bo_csv_url_groups,"output_path":paths.groups_csv_path,"table_name":"GROUPS FULL","db_script_function":"refresh_db_groups"},
+    "invoices_full":{"csv_url":paths.bo_csv_url_invoices,"output_path":paths.invoices_csv_path,"table_name":"INVOICES FULL","db_script_function":"refresh_db_invoices"},
+    "students_full":{"csv_url":paths.bo_csv_url_students,"output_path":paths.students_csv_path,"table_name":"STUDENTS FULL","db_script_function":"refresh_db_students"},
+    "events_filter":{"csv_url":paths.bo_csv_url_events_filter,"output_path":paths.events_filter_csv_path,"table_name":"EVENTS UPDATES","db_script_function":"update_db_events"},
+    "invoices_filter":{"csv_url":get_urls("invoices_filter"),"output_path":paths.invoices_filter_csv_path,"table_name":"INVOICES UPDATES","db_script_function":"update_db_invocies"},
+    "students_filter":{"csv_url":get_urls("students_filter"),"output_path":paths.students_filter_csv_path,"table_name":"STUDENTS UPDATES","db_script_function":"update_db_students"},
+    "leads_full":{"csv_url":None,"output_path":paths.leads_csv_path,"table_name":"LEADS FULL","db_script_function":"refresh_db_leads","source":"AMO"},
+    "budgets_full":{"csv_url":None,"output_path":paths.budgets_csv_path,"table_name":"BUDGETS FULL","db_script_function":"refresh_db_budgets","source":"GCP"},
+    "payments_full":{"csv_url":paths.bo_csv_url_payments,"output_path":paths.payments_csv_path,"table_name":"PAYMENTS FULL","db_script_function":"refresh_db_payments"},
+    "payments_filter":{"csv_url":get_urls("payments_filter"),"output_path":paths.payments_filter_csv_path,"table_name":"PAYMENTS UPDATES","db_script_function":"update_db_payments"}
 }
 
 
@@ -68,7 +59,7 @@ if __name__ == "__main__":
         else:
             print(f"Invalid parameter set name: {parameter_set_name}")
     else:
-        print("Usage: python your_script_name.py [events_full|groups_full|invoices_full|students_full|events_filter|invoices_filter|students_filter|leads_full|budgets_full]. Add '_silent' to the parameter to suspend the telegram notification. For example 'events_full_silent'")
+        print("Usage: python your_script_name.py [events_full|groups_full|invoices_full|students_full|events_filter|invoices_filter|students_filter|leads_full|budgets_full|payments_full|payments_filter]")
 
 
 # Test run
@@ -76,9 +67,11 @@ if __name__ == "__main__":
 # update_table(**PARAMETER_SETS["groups_full"])
 # update_table(**PARAMETER_SETS["invoices_full"])
 # update_table(**PARAMETER_SETS["students_full"])
+# update_table(**PARAMETER_SETS["events_filter"])
+# update_table(**PARAMETER_SETS["invoices_filter"])
+# update_table(**PARAMETER_SETS["students_filter"])
 # update_table(**PARAMETER_SETS["leads_full"])
 # update_table(**PARAMETER_SETS["budgets_full"])
 # update_table(**PARAMETER_SETS["payments_full"])
 # update_table(**PARAMETER_SETS["payments_filter"])
-# update_table(**PARAMETER_SETS["invoices_filter"])
-# update_table(**PARAMETER_SETS["students_filter"])
+
