d429104bf6dd11129e27c0f258afff955ff41619
Dalamat <squalldmitri@gmail.com>
2023-11-04 13:16:58 +0300

    style fixes

--- telegram_client.py
+++ telegram_client.py
@@ -10,5 +10,5 @@ async def send_group_message(text: str):
     bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=GROUP_ID, text=text)
 
-#Test function call
-#asyncio.run(send_group_message("Test"+" "+"Test2"))
+# Test function call
+# asyncio.run(send_group_message("Test"+" "+"Test2"))
